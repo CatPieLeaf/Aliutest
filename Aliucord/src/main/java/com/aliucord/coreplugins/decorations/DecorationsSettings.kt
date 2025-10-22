9563e451c12942cf54745d918ea67cdb5554e9fa
Cilly Leang <me@lava.moe>
2025-10-23 01:22:49 +1100

    feat(Decorations): implement Decorator API

--- Aliucord/src/main/java/com/aliucord/coreplugins/decorations/DecorationsSettings.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/decorations/DecorationsSettings.kt
@@ -5,21 +5,21 @@ import android.view.View
 import com.aliucord.Utils
 import com.aliucord.api.SettingsAPI
 import com.aliucord.settings.SettingsDelegate
-import com.aliucord.settings.delegate
 import com.aliucord.widgets.BottomSheet
 import com.discord.views.CheckedSetting
 
 internal object DecorationsSettings {
     private val settings = SettingsAPI("Decorations")
 
-    // TODO: remove this
-    // This exists to allow enabling for testing by manually editing the json settings
-    val enable by settings.delegate(false)
+    // val enableAvatarDecoDelegate = settings.delegate("enableAvatarDecorations", true)
+    // val enableAvatarDeco by enableAvatarDecoDelegate
 
     @Suppress("MISSING_DEPENDENCY_CLASS", "MISSING_DEPENDENCY_SUPERCLASS")
     class Sheet : BottomSheet() {
         override fun onViewCreated(view: View, bundle: Bundle?) {
             super.onViewCreated(view, bundle)
+
+            // createSetting(enableAvatarDecoDelegate)
         }
 
         private fun createSetting(description: String, delegate: SettingsDelegate<Boolean>): CheckedSetting {
@@ -32,6 +32,7 @@ internal object DecorationsSettings {
                 var setting by delegate
                 isChecked = setting
                 setOnCheckedListener { setting = !setting }
+                Utils.promptRestart()
             }
         }
     }
